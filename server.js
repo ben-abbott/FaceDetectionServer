88d6dcf2ae312c2fafe89ed895ed7598a8593619
Benjamin Abbott <ben_abbott@outlook.com>
2023-04-28 16:12:18 +0100

    fixed db connection issue

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-
+const knex = require('knex');
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'test',
+      database : 'smart-brain'
+    }
+});
+
+
+
 
 
 
@@ -35,9 +53,7 @@ const database = {
     ]
 }
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.send(database.users)
@@ -54,14 +70,15 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
+    db('users').returning('*').insert({
         email: email,
-        entries: 0,
+        name: name,
         joined: new Date()
-    });
-    res.json(database.users[database.users.length-1])
+    })
+    .then(user => {
+        res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('Unable to register'))
 });
 
 app.get('/profile/:id', (req, res) => {
